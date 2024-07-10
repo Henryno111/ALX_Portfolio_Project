0734852af596cebb8fcce41fe0f2a0072bb88d95
Henryno111 <h.agukwe@yahoo.com>
2024-07-10 09:30:07 +0100

    modification to posts controller due to error when making a post in the frontend

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -1,6 +1,5 @@
-import Post from '../models/Post.js';
-import User from '../models/User.js';
-
+import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 /* CREATE */
 export const createPost = async (req, res) => {
@@ -11,15 +10,16 @@ export const createPost = async (req, res) => {
       userId,
       firstName: user.firstName,
       lastName: user.lastName,
+      location: user.location,
       description,
       userPicturePath: user.picturePath,
       picturePath,
       likes: {},
       comments: [],
-    })
+    });
     await newPost.save();
 
-    const post = await post.find();
+    const post = await Post.find();
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
@@ -32,7 +32,7 @@ export const getFeedPosts = async (req, res) => {
     const post = await Post.find();
     res.status(200).json(post);
   } catch (err) {
-    res.status(404).json({ message: err.message })
+    res.status(404).json({ message: err.message });
   }
 };
 
@@ -42,7 +42,7 @@ export const getUserPosts = async (req, res) => {
     const post = await Post.find({ userId });
     res.status(200).json(post);
   } catch (err) {
-    res.status(404).json({ message: err.message })
+    res.status(404).json({ message: err.message });
   }
 };
 
@@ -52,7 +52,7 @@ export const likePost = async (req, res) => {
     const { id } = req.params;
     const { userId } = req.body;
     const post = await Post.findById(id);
-    const isLiked = post.likes[userId];
+    const isLiked = post.likes.get(userId);
 
     if (isLiked) {
       post.likes.delete(userId);
@@ -60,13 +60,13 @@ export const likePost = async (req, res) => {
       post.likes.set(userId, true);
     }
 
-    const updatedPost = await Post.findIdAndUpdate(
+    const updatedPost = await Post.findByIdAndUpdate(
       id,
       { likes: post.likes },
       { new: true }
     );
 
-    res.status(200).json
+    res.status(200).json(updatedPost);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
